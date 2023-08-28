780e567e623937e50db6f1d6c14c0d02f68410fe
Sean-C45 <32867824@student.g.nwu.ac.za>
2023-08-28 17:21:22 +0200

    this is my developer branch which is different from the main branch as i push the development into this branch and the final would be pushed into my main branch

--- Project2APICreation/Project2APICreation/Program.cs
+++ Project2APICreation/Project2APICreation/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Project2APICreation.Models;
+using System.Configuration;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<cmpg323SeanContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,9 +26,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+
 
 app.MapControllers();
 
